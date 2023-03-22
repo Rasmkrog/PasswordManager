64add6478aa65e11cb44d1c9201a65fdfd0544af
Krog <Rasmkrog@gmail.com>
2023-03-22 12:33:22 +0100

    Added Core cs files to solutions Added more folders for organisation purpose

---
+++ PasswordManager/PasswordManager/Core/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PasswordManager.Core;
+
+class RelayCommand : ICommand
+{
+    
+    private Action<Object> execute;
+    private Func<object, bool> canExecute;
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    protected RelayCommand(Action<Object> execute, Func<object, bool> canExecute)
+    {
+        this.execute = execute;
+        this.canExecute = canExecute;
+    }
+    
+    public bool CanExecute(object? parameter)
+    {
+        return parameter != null && this.canExecute(parameter);
+    }
+    
+    public void Execute(object? parameter)
+    {
+        if (parameter != null) this.execute(parameter);
+    }
+}
