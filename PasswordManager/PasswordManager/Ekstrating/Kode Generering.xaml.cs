96d6fbdc2777fd395b5465aa1a70fdcf1b017048
Peter <peterbrams@live.dk>
2023-05-03 23:13:59 +0200

    Videre med kode gen

--- PasswordManager/PasswordManager/Ekstrating/Kode Generering.xaml.cs
+++ PasswordManager/PasswordManager/Ekstrating/Kode Generering.xaml.cs
@@ -58,8 +58,25 @@ public partial class Kode_Generering : Page
             return "sda";
         }
 
-        void GetPass()
+        public  static  void GetPass()
         {
+            Random res = new Random();
+                //denne skal laves om så den kan bruge det over
+               String add = "abcdefghijklmnopqrstuvwxyz1234567890?!@#$%^&*";
+            int size = 10;
             
+            //Den string den der viser kode når det er sat ind 
+            String ran = "";
+
+            for (int i = 0; i < size; i++)
+            {
+                //så kan den vælge all char 
+                int x = res.Next(26);
+
+                ran = ran + add[x];
+            }
+
+
+
         }
 }
