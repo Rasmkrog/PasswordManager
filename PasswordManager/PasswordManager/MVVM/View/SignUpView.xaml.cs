ce7826546e4c41a4e7e9828c1a0837f4ad5587c2
nikol <nikolaj.h.koch@gmail.com>
2023-05-04 00:14:48 +0200

    backend til signup

--- PasswordManager/PasswordManager/MVVM/View/SignUpView.xaml.cs
+++ PasswordManager/PasswordManager/MVVM/View/SignUpView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Controls;
 
 namespace PasswordManager.MVVM.View;
 
@@ -7,14 +10,33 @@ public partial class SignUpView : Page
     public SignUpView()
     {
         InitializeComponent();
+        binddatagrid();
+    }
+
+    private void binddatagrid() {
+        //Skaber forbindelse til SQL database gennem "ConnectionString"
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = ConfigurationManager.ConnectionStrings["DBConnection.ConnectionString"].ConnectionString;
+        
+        //Indsætter brugernavn i database
+        con.Open();
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "select * from[Username]";    //tjek navn
+        cmd.Connection = con;
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataTable dt = new DataTable("Username");       //samme her, samme navn tho
+        da.Fill(dt);
+        
     }
 
     private string brugernavnText;
     private string kodeordText;
 
+    
     public void SignUp()
     {
         brugernavnText = brugernavn.Text;
         kodeordText = kodeord.Text;
     }
+
 }
