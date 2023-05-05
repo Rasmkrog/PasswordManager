7a3654d1b74ff52790ff883846fecc766edd24af
Rasmkrog <Rasmkrog@gmail.com>
2023-05-05 17:57:17 +0200

    added username

--- PasswordManager/PasswordManager/Core/UserInfo.cs
+++ PasswordManager/PasswordManager/Core/UserInfo.cs
@@ -3,7 +3,7 @@ namespace PasswordManager.Core;
 
 public class UserInfo
 {
-    public string? UserName { get; set; }
+    public static string Username { get; set; }
 
     // ReSharper disable once InconsistentNaming
     public static int? UserID { get; set; } = 4;
