f5287bb924cb5cac2048fd230cb482293c70195f
nikol <nikolaj.h.koch@gmail.com>
2023-05-05 17:58:35 +0200

    :)

--- PasswordManager/PasswordManager/Core/UserInfo.cs
+++ PasswordManager/PasswordManager/Core/UserInfo.cs
@@ -3,7 +3,7 @@ namespace PasswordManager.Core;
 
 public class UserInfo
 {
-    public static string Username { get; set; }
+    public static string? UserName { get; set; }
 
     // ReSharper disable once InconsistentNaming
     public static int? UserID { get; set; } = 4;
