d340d11a9d1b3c873a82161f4892d9a786f3d531
Peter <peterbrams@live.dk>
2023-05-06 13:25:30 +0200

    Kommenteret kode

--- PasswordManager/PasswordManager/MVVM/View/PasswordGenView.xaml.cs
+++ PasswordManager/PasswordManager/MVVM/View/PasswordGenView.xaml.cs
@@ -16,6 +16,7 @@ public partial class PasswordGenView : UserControl
     }
     public static class PasswordGenerator
     {
+        //oprettelse af variabler
         private const string LowercaseChars = "abcdefghijklmnopqrstuvwxyz";
         private const string UppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         private const string DigitChars = "0123456789";
@@ -23,13 +24,11 @@ public partial class PasswordGenView : UserControl
 
         public static string Generate(int length, bool includeLowercase, bool includeUppercase, bool includeDigits, bool includeSpecialChars)
         {
-            if (length < 1)
-            {
-                throw new ArgumentException("Length must be greater than or equal to 1");
-            }
-
+            
+                //en tom variabel som bliver lavet 
             var allowedChars = "";
 
+                // hvis IncludeLowercase bliver valgt så skal den med i allowedchars
             if (includeLowercase)
             {
                 allowedChars += LowercaseChars;
@@ -49,39 +48,42 @@ public partial class PasswordGenView : UserControl
             {
                 allowedChars += SpecialChars;
             }
-
-            if (allowedChars.Length == 0)
-            {
-                throw new ArgumentException("At least one character set must be selected");
-            }
-
+            
+            // Opretter en char array til at holde resultatet
             var result = new char[length];
-
+            
+            // Opret en kryptografisk tilfældighes generator. Så den kan vælge forskellige char
             using (var rng = new RNGCryptoServiceProvider())
             {
                 byte[] bytes = new byte[sizeof(uint)];
-
+                
+                //loop gennem den givende længde af kodeordet som er blevet indsat 
                 for (int i = 0; i < length; i++)
                 {
+                    // Generer en tilfældig uint-værdi. Grunden til vi bruger ToUInt istedetfor ToInt er fordi at UInt sætter det til unsigned
                     rng.GetBytes(bytes);
                     uint value = BitConverter.ToUInt32(bytes, 0);
+                   
+                    //bestem af hvilke kaaraktere der skal skal bruges til og lave kode ved hjælp af den tilfældige uint-værdi ved de tilladte karakter 
                     int index = (int)(value % (uint)allowedChars.Length);
                     result[i] = allowedChars[index];
                 }
             }
-
+            // Konverter resultatet til en streng og returner det
             return new string(result);
         }
     }
         
     private void GeneratePasswordButton_Click(object sender, RoutedEventArgs e)
     {
+        //henter brugers valg efter om de har checket af ved Checkbox 
         int length = int.Parse(LengthTextBox.Text);
         bool includeLowercase = LowercaseCheckBox.IsChecked.Value;
         bool includeUppercase = UppercaseCheckBox.IsChecked.Value;
         bool includeDigits = DigitsCheckBox.IsChecked.Value;
         bool includeSpecialChars = SpecialCharsCheckBox.IsChecked.Value;
 
+        // Generer et kodeord baseret på brugerens valg
         var password = PasswordGenerator.Generate(length, includeLowercase, includeUppercase, includeDigits, includeSpecialChars);
 
         PasswordTextBox.Text = password;
